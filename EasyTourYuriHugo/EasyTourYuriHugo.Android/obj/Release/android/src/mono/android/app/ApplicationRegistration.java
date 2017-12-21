55c6bb7965ce5f4a6750911d2dc752cf4966715f
easytourbrasil <easytourbrasil.rn@gmail.com>
2017-12-20 23:27:46 -0300

    Update

---
+++ EasyTourYuriHugo/EasyTourYuriHugo.Android/obj/Release/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("EasyTourYuriHugo.Droid.MainApplication, EasyTourYuriHugo.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md50dfec7a7e1f3b23645c73ec2f0d3d268.MainApplication.class, md50dfec7a7e1f3b23645c73ec2f0d3d268.MainApplication.__md_methods);
+		
+	}
+}
